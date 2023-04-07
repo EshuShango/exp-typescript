14c0ee63ce68615d28c3977c60ef8d2211e646db
EshuShango <tobiakinyemi21@gmail.com>
2023-04-07 00:29:35 -0500

    quick upd

---
+++ 02basics/myObj.js
@@ -0,0 +1,14 @@
+"use strict";
+// const User = {
+//   name: "John",
+//   email: "john@gmail.com",
+//   isActive: true
+// }
+exports.__esModule = true;
+var myUser = {
+    _id: "123",
+    name: "John",
+    email: "j@j.com",
+    isActive: false
+};
+myUser.email = "t@t.com";
--- 02basics/myObj.ts
+++ 02basics/myObj.ts
@@ -20,19 +20,69 @@
 // function createCourse():{name: string, price: number}{
 //   return {name: "react", price: 1000};
 // }
-//^below: can help to pass only the required properties
-type User = {
+
+
+//!-----------------
+// //^below: can help to pass only the required properties, we use an Alias
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// }
+
+
+
+// function createUser(user: User): User{
+//   return {name: "", email: "", isActive: true}
+// };
+
+// createUser({name:"", email:"", isActive: true});
+//!_________________
+
+
+
+
+//!-----------------
+//^below: the readonly keyword makes the property immutable
+type User ={
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  //^below: the ? makes the property optional
+  creditcardDetails?: number
 }
 
-
-
-function createUser(user: User): User{
-  return {name: "", email: "", isActive: true}
+let myUser: User = {
+  _id: "123",
+  name: "John",
+  email: "j@j.com",
+  isActive: false,
+  
 };
 
-createUser({name:"", email:"", isActive: true});
+//^below: Credit Card
+
+type cardNumber = {
+  cardNumber: string
+};
+
+type cardDate = {
+  cardDate: string
+};
+//^below: the & is a union type
+type cardDetails = cardNumber & cardDate & {
+  cvv: number
+}; 
+
+
+
+myUser.email = "t@t.com";
+
+//^below: will throw an error, because _id is immutable
+// myUser._id = "afafads"
+
+
+//!_________________
 
 export {}
